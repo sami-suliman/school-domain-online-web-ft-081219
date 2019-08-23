38a62947f965c0040e2029214a1816aaa8ca04c0
sami suliman <sami.bco@gmail.com>
2019-08-23 16:07:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School 
+ attr_accessor :name, :roster
+  
+ 
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    if @roster[grade]
+      @roster[grade] << student
+    else 
+      @roster[grade] = []
+    end 
+  end 
+end 
+
+#school = School.new("Bayside High School")
