94b15deaf4ee3ecb23935f7f78b6552226c40ff1
sami suliman <sami.bco@gmail.com>
2019-08-23 16:38:56 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+require "pry"
+class School 
+ attr_accessor :name, :roster
+  
+ 
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    if @roster[grade]
+      @roster[grade] << student
+    else 
+      @roster[grade] = []
+      @roster[grade] << student
+    end 
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort 
+    #binding.pry
+    @roster.each do |grade, names|
+      names.sort!
+    end 
+  end
+end 
+
+#school = School.new("Bayside High School")
